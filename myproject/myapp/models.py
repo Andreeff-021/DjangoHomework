283f803c32c67e74b7b8e14ed078e48d1daa4399
andre <andreeff.vladimir@yandex.ru>
2023-10-04 10:53:38 +0300

    homework_5

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class User(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField()
@@ -13,12 +20,13 @@ class User(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    count = models.IntegerField(default=0)
-    date_add = models.DateField(auto_now_add=True)
-    image = models.ImageField()
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(default='', blank=True)
+    price = models.DecimalField(default=999999.99, max_digits=8, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+    rating = models.DecimalField(default=5.0, max_digits=3, decimal_places=2)
 
     def __str__(self):
         return f'{self.name}'
